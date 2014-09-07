941b86255567f864e60eb0291e8589361659b72e
Raphael Amorim <rapha850@gmail.com>
2014-09-07 00:50:15 -0300

    Support Jade Template Engine

--- kenobi.js
+++ kenobi.js
@@ -2,10 +2,18 @@ var Kenobi = (function(req, path, callback) {
 	var request = require('request'),
 		fs = require('fs'),
 		ejs = require('ejs'),
-		str = fs.readFileSync(__dirname + '/../..' + path, 'utf8');
+		jade = require('jade'),
+		fileExt = path.split('.').pop(),
+		realPath = __dirname + '/../..' + path;
+		str = fs.readFileSync(realPath, 'utf8');
 
 	request(req, function(error, response, body) {
-	 	return callback(ejs.render(str, {body: body}));
+		if (fileExt === 'ejs')
+	 		return callback(ejs.render(str, {body: body}));
+	 	else if (fileExt === 'jade')
+	 		return callback(jade.renderFile(realPath, {body: body}));
+	 	else
+	 		return false;
 	});
 });
 
