27977759968a233986c061ab83f753bd9b973823
Raphael Amorim <rapha850@gmail.com>
2015-02-23 06:19:00 +0100

    Update tests

--- test/request.js
+++ test/request.js
@@ -2,12 +2,16 @@ var kenobi = require('../kenobi'),
 	nock   = require('nock'),
 	assert = require('assert');
 
+// TODO: test ejs, jade, html render
+
 describe('Requests', function() {
 	context('Valid requests', function() {
 		context('Url as param', function() {
 			it('should get response without view', function(done) {
-				var url = 'http://test.com';
-				var api = nock(url).get('/').reply(200, {'data': true});
+				var url = 'http://test.com',
+					api = nock(url)
+						  .get('/')
+						  .reply(200, {'data': true});
 
 				kenobi(url, function(res, err){
 					assert.equal(res.statusCode, 200);
@@ -33,7 +37,7 @@ describe('Requests', function() {
 		});
 		context('Invalid url as param', function() {
 			it('should get error', function(done) {
-				kenobi('InvalidParam', function(res, err){
+				kenobi(null, function(res, err){
 					assert.equal(typeof res, 'undefined');
 					assert.equal(typeof err, 'object');
 
